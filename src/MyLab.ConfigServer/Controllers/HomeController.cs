d4b9a8504e4e32ff85a52a8003437040e7a05ca0
Andrey O <ozzy@triasoft.com>
2020-03-09 20:15:40 +0300

    Add custom base path support + light logging

--- src/MyLab.ConfigServer/Controllers/HomeController.cs
+++ src/MyLab.ConfigServer/Controllers/HomeController.cs
@@ -4,7 +4,9 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using MyLab.ConfigServer.Models;
 using MyLab.ConfigServer.Services;
 
@@ -12,13 +14,15 @@ namespace MyLab.ConfigServer.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
         public IConfigProvider ConfigProvider { get; }
 
         /// <summary>
         /// Initializes a new instance of <see cref="HomeController"/>
         /// </summary>
-        public HomeController(IConfigProvider configProvider)
+        public HomeController(IConfigProvider configProvider, ILogger<HomeController> logger)
         {
+            _logger = logger;
             ConfigProvider = configProvider;
         }
 
@@ -30,7 +34,21 @@ namespace MyLab.ConfigServer.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            LogError();
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private void LogError()
+        {
+            var exceptionHandlerPathFeature =
+                HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            string msg = exceptionHandlerPathFeature?.Error is Exception ex
+                ? ex.Message
+                : "Request handle error";
+
+            _logger.LogError($"{msg}. Path:'{exceptionHandlerPathFeature?.Path}'");
         }
     }
 }

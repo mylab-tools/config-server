16452e84a132305c2750982ac265bc457ae2e1a5
Andrey O <ozzy.ext@gmail.com>
2021-07-08 20:08:05 +0300

    fix publish

--- ci/publish.bat
+++ ci/publish.bat
@@ -3,13 +3,13 @@
 IF [%1]==[] goto noparam
 
 echo "Build image '%1' and 'latest'..."
-docker build --progress=plain -f .\Dockerfile -t mylabtools/config-server:%1 -t mylabtools/config-server:latest ..\..
+docker build --progress=plain -f .\Dockerfile --build-arg PROJ_VER=%1 -t mylabtools/config-server:%1 -t mylabtools/config-server:latest ..
 
 echo "Publish image '%1' ..."
-rem docker push mylabtools/config-server-ui:%1
+docker push mylabtools/config-server:%1
 
 echo "Publish image 'latest' ..."
-rem docker push mylabtools/config-server-ui:latest
+docker push mylabtools/config-server:latest
 
 goto done
 
